3ed6a2cca9715202cffdf2271c187e27c9e6f866
thamiristrentin <thamiristrentinads@gmail.com>
2024-12-31 03:07:52 -0300

    att 30/dez/2024

---
+++ css/node_b4d3090910ff6.tmp.js
@@ -0,0 +1,78 @@
+#hero-section {
+    background-color: var(--amarelo);
+    min-width: 100%;
+}
+
+.hero-section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding-top: 3.43rem;
+}
+
+.content-herosection h1 {
+    font: 4.68rem "ExtraBold", sans-serif;
+    color: var(--azulescuro);
+}
+
+.content-herosection h2 {
+    font: 2.18rem "SemiBold", sans-serif;
+    color: var(--azulclaro);
+}
+
+.content-herosection button {
+    font: 1.56rem "SemiBold", sans-serif;
+    color: var(--branco);
+    border: 0;
+    background-color: var(--azulescuro);
+    padding: 0.75rem 3.37rem;
+    border-radius: 24px;
+}
+
+.botoes-herosection {
+    padding-top: 1.81rem;
+    display: flex;
+    gap: 2rem;
+}
+
+.botao-hs {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.botao-hs img {
+    padding-right: 0.93rem;
+}
+
+.content-herosection p {
+    font: 1.93rem "Regular", sans-serif;
+    color: var(--azulescuro);
+    padding-top: 3.31rem;
+    max-width: 41.81rem;
+}
+
+.texto-about {
+    font: 1.93rem "Bold", sans-serif;
+    color: var(--azulescuro);
+    padding-top: 8.25rem;
+    text-align: center;
+}
+
+.diferenciais {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: ;
+    padding-top: 6.12rem;
+}
+
+.diferencial {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.diferencial img {
+    max-width: max-content;
+}
